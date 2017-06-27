08b4fe473c1715caa17e31ab9250570f4ba6fb50
Meterien <meterien@gmail.com>
2017-06-27 16:15:30 -0400

    Add a compliance item to remove detect and remove Quicktime

---
+++ Compliance/App - Remove Quicktime.ps1
@@ -0,0 +1,15 @@
+﻿# Détection
+$QuicktimeInstances = @(Get-WmiObject -Namespace root/cimv2/sms -Class sms_installedsoftware -Filter "ProductName LIKE '%QuickTime%'")
+if($QuicktimeInstances.Count -gt 0) {
+    return 'NonCompliant'
+}
+else {
+    return 'Compliant'
+}
+
+# Correction
+$QuicktimeInstances = @(Get-WmiObject -Namespace root/cimv2/sms -Class sms_installedsoftware -Filter "ProductName LIKE '%QuickTime%'")
+foreach($Instance in $QuicktimeInstances) {
+    Write-Host "$($Instance.ProductName) : $($Instance.ProductVersion)"
+    $null = Start-Process -FilePath msiexec -ArgumentList "/X $($Instance.SoftwareCode) REBOOT=ReallySuppress /QN" -Wait -Verb RunAs -WindowStyle Hidden
+}
